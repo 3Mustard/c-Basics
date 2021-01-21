ebf6d852db96a381e8371955618c41e32f975318
Matthew Cullen <m23.cullen@gmail.com>
2021-01-21 17:41:51 -0500

    first commit

---
+++ GreetingProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GreetingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting(GetData());
+        }
+
+        private static string[] GetData()
+        {
+            Console.WriteLine("What is your name and age? ");
+            string[] data = Console.ReadLine().Split(' ');
+            
+            return data;
+        }
+
+        private static string GetAgeInXYears(string age, int years)
+        {
+            int parsedAge = Int32.Parse(age);
+            return (parsedAge + years).ToString();
+        }
+
+        private static void Greeting(String[] data)
+        {
+            string name = data[0];
+            string age = data[1];
+            string ageInTen = GetAgeInXYears(data[1], 10);
+
+            Console.WriteLine($"Hello {name}, you are {age} years old!");
+            Console.WriteLine($"In ten years you will be {ageInTen}");
+        }
+    }
+}
